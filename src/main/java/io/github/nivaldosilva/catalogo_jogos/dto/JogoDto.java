57625e66f463765419f04596ecc7892cc3fec32c
Nivaldo Silva <nivaldosilva.contato@gmail.com>
2026-05-23 06:58:45 -0300

    feat(dto): atualiza JogoDto e mapeamento para criterios de busca.

--- src/main/java/io/github/nivaldosilva/catalogo_jogos/dto/JogoDto.java
+++ src/main/java/io/github/nivaldosilva/catalogo_jogos/dto/JogoDto.java
@@ -12,24 +12,26 @@ import java.util.UUID;
 public class JogoDto {
 
 	@Builder
-	public record jogoRequest(@NotBlank @Size(max = 150) String nome,
-							  @Size(max = 500) String descricao,
-							  @NotNull LocalDate dataLancamento,
-							  @Size(max = 100) String desenvolvedora,
-							  @Size(max = 500) String urlDaImagem,
-							  List<UUID> consolesIds,
-							  List<UUID> categoriasIds
+	public record jogoRequest(
+			@NotBlank @Size(max = 150) String nome,
+			@Size(max = 500) String descricao,
+			@NotNull LocalDate dataLancamento,
+			@Size(max = 100) String desenvolvedora,
+			@Size(max = 255) String urlDaImagem,
+			List<UUID> consolesIds,
+			List<UUID> categoriasIds
 	) {}
 
 	@Builder
 	@JsonInclude(JsonInclude.Include.NON_NULL)
-	public record jogoResponse(UUID id,
-							   String nome,
-							   String descricao,
-							   LocalDate dataLancamento,
-							   String desenvolvedora,
-							   String urlDaImagem,
-							   List<ConsoleDto.consoleResponse> consoles,
-							   List<CategoriaDto.categoriaResponse> categorias
+	public record jogoResponse(
+			UUID id,
+			String nome,
+			String descricao,
+			LocalDate dataLancamento,
+			String desenvolvedora,
+			String urlDaImagem,
+			List<ConsoleDto.consoleResponse> consoles,
+			List<CategoriaDto.categoriaResponse> categorias
 	) {}
 }
--- src/main/java/io/github/nivaldosilva/catalogo_jogos/mapper/JogoMapper.java
+++ src/main/java/io/github/nivaldosilva/catalogo_jogos/mapper/JogoMapper.java
@@ -1,24 +1,40 @@
 package io.github.nivaldosilva.catalogo_jogos.mapper;
 
+import io.github.nivaldosilva.catalogo_jogos.dto.CategoriaDto;
+import io.github.nivaldosilva.catalogo_jogos.dto.ConsoleDto;
 import io.github.nivaldosilva.catalogo_jogos.dto.JogoDto;
+import io.github.nivaldosilva.catalogo_jogos.entity.Categoria;
+import io.github.nivaldosilva.catalogo_jogos.entity.Console;
 import io.github.nivaldosilva.catalogo_jogos.entity.Jogo;
 import lombok.experimental.UtilityClass;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @UtilityClass
 public class JogoMapper {
 
-	public Jogo toEntity(JogoDto.jogoRequest request, String urlDaImagem) {
+	public Jogo toEntity(JogoDto.jogoRequest request, List<Console> consoles, List<Categoria> categorias) {
 		return Jogo.builder()
 				.nome(request.nome())
 				.descricao(request.descricao())
 				.dataLancamento(request.dataLancamento())
 				.desenvolvedora(request.desenvolvedora())
-				.urlDaImagem(urlDaImagem)
+				.urlDaImagem(request.urlDaImagem())
+				.consoles(consoles)
+				.categorias(categorias)
 				.build();
 	}
 
 	public JogoDto.jogoResponse toResponse(Jogo jogo) {
+		List<ConsoleDto.consoleResponse> consoles = jogo.getConsoles() == null ? List.of() :
+				jogo.getConsoles().stream()
+						.map(ConsoleMapper::toResponse)
+						.toList();
+
+		List<CategoriaDto.categoriaResponse> categorias = jogo.getCategorias() == null ? List.of() :
+				jogo.getCategorias().stream()
+						.map(CategoriaMapper::toResponse)
+						.toList();
+
 		return JogoDto.jogoResponse.builder()
 				.id(jogo.getId())
 				.nome(jogo.getNome())
@@ -26,12 +42,8 @@ public class JogoMapper {
 				.dataLancamento(jogo.getDataLancamento())
 				.desenvolvedora(jogo.getDesenvolvedora())
 				.urlDaImagem(jogo.getUrlDaImagem())
-				.consoles(jogo.getConsoles().stream()
-						.map(ConsoleMapper::toResponse)
-						.collect(Collectors.toList()))
-				.categorias(jogo.getCategorias().stream()
-						.map(CategoriaMapper::toResponse)
-						.collect(Collectors.toList()))
+				.consoles(consoles)
+				.categorias(categorias)
 				.build();
 	}
 }
